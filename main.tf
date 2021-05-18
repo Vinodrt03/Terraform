0eacec2493b25fdebf5afb1d1af4db1ced3ecc16
Vinodrt03 <79192584+Vinodrt03@users.noreply.github.com>
2021-05-18 11:17:11 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+access_key = "AKIAUWGMV37N5JOKMWHW"
+secret_key = "jzY00p0CrLWCR9UJCP0+v0YWbeWLQixZxpWkAWa8"
+region = "ap-south-1"
+}
+resource "aws_instance" "example" {
+ami = "ami-010aff33ed5991201"
+instance_type = "t2.micro"
+}
