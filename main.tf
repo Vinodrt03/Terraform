e99b9e3e62d43fce7024cfab997490d174e578f7
Vinodrt03 <79192584+Vinodrt03@users.noreply.github.com>
2021-05-24 16:10:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,7 @@ access_key = "AKIAUWGMV37N76BT3UPA"
 secret_key = "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"
 region = "ap-south-1"
 }
-
-resource "aws_instance" "vinod" {
+resource "aws_instance" "Web_server" {
 ami = "ami-010aff33ed5991201"
 instance_type = "t2.micro"
 }
