10888238530a1d5e32d6c1dd5dc79584a2c19b7d
Vinodrt03 <79192584+Vinodrt03@users.noreply.github.com>
2021-05-24 16:24:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-access_key = "AKIAUWGMV37N76BT3UPA"
-secret_key = "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"
+access_key = "AKIAUWGMV37NSIXB2HBJ"
+secret_key = "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"
 region = "ap-south-1"
 }
 resource "aws_instance" "Web_server" {
