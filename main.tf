b353dc1e4ba51ce164bfb47ec292a3fe6be5d2bf
Vinodrt03 <79192584+Vinodrt03@users.noreply.github.com>
2021-05-24 15:13:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ secret_key = "jzY00p0CrLWCR9UJCP0+v0YWbeWLQixZxpWkAWa8"
 region = "ap-south-1"
 }
 resource "aws_instance" "example" {
-ami = "ami-010aff33ed5991201"
+ami = "ami-040589e8273e08d6e"
 instance_type = "t2.micro"
 }
